d45cb1a6b4b0462fa3c46938ecb202785160139b
Aleks <nikolic.aleks90@gmail.com>
2020-05-01 18:36:01 -0400

    Updates

---
+++ precache-manifest.f28a0cdbd8b1cfe25dd24e2c5c307ffe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c761c227260484af9221b3237e1b958c",
+    "url": "/aleksdoesit/AleksandarNikolicProjectFive/index.html"
+  },
+  {
+    "revision": "cf84a7ad17e9537a4eba",
+    "url": "/aleksdoesit/AleksandarNikolicProjectFive/static/css/main.e3aaefd6.chunk.css"
+  },
+  {
+    "revision": "b6c1f8edc5bd8d9d2b13",
+    "url": "/aleksdoesit/AleksandarNikolicProjectFive/static/js/2.859b57d1.chunk.js"
+  },
+  {
+    "revision": "4ae2e24854201e2c90f766e681fee893",
+    "url": "/aleksdoesit/AleksandarNikolicProjectFive/static/js/2.859b57d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf84a7ad17e9537a4eba",
+    "url": "/aleksdoesit/AleksandarNikolicProjectFive/static/js/main.94087d89.chunk.js"
+  },
+  {
+    "revision": "1d64f7e10ad768107279",
+    "url": "/aleksdoesit/AleksandarNikolicProjectFive/static/js/runtime-main.0e34de61.js"
+  }
+]);
